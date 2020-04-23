6bf201bda8c8cb254fa12ae1ebb4d3a290344833
Eric Park <43224005+parkeric1998@users.noreply.github.com>
2020-04-22 20:05:20 -0400

    Finished quizzes component

--- controllers/quizzes.controller.server.js
+++ controllers/quizzes.controller.server.js
@@ -2,10 +2,12 @@ const quizzesService = require('../services/quizzes.services.service')
 
 module.exports = function(app){
     app.get('/api/quizzes',(req,res) =>
-        res.json(quizzesService.findAllQuizzes()))
+        res.json(quizzesService
+            .findAllQuizzes()))
 
     app.get('/api/quizzes/:qzid',(req,res) =>
-        res.json(quizzesService.findQuizById(req.params['qzid']))
+        res.json(quizzesService
+            .findQuizById(req.params['qzid']))
     )
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
 
-app.get('/hello',(req,res) =>
-    res.send('hello world'))
 app.listen(3000)
